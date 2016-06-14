da3ba2070b3a79697e2eb8dd0b2b8713137eabdc
Jackymancs4 <jackyman_cs4@live.it>
2016-06-14 21:55:31 +0200

    Linted and indented code.

--- git-streak.py
+++ git-streak.py
@@ -1,38 +1,38 @@
 import datetime
 
-tdate=datetime.datetime.today()
-tdatebase=datetime.date.today()
-ttime=datetime.time()
+tdate = datetime.datetime.today()
+tdatebase = datetime.date.today()
+ttime = datetime.time()
 
-#print tdate
+# print tdate
 
-def formatter (datevar):
-	return datevar.strftime('%a %b %d %X %Y +0100')
+def formatter(datevar):
+    return datevar.strftime('%a %b %d %X %Y +0100')
 
-def print_format (datevar):
-	print formatter(datevar)
 
-year_delta=datetime.timedelta(days=365)
-day_delta=datetime.timedelta(days=1)
-hour_delta=datetime.timedelta(hours=2, minutes=30)
+def print_format(datevar):
+    print formatter(datevar)
 
-first_date=(tdatebase-year_delta)
+year_delta = datetime.timedelta(days=365)
+day_delta = datetime.timedelta(days=1)
+hour_delta = datetime.timedelta(hours=2, minutes=30)
 
-new_day=first_date
+first_date = (tdatebase - year_delta)
 
-for x in range(1,365):
-	new_day=(new_day+day_delta)
-	new_baseday=datetime.datetime.combine(new_day, ttime)
-	new_baseday=(new_baseday+hour_delta+hour_delta)
+new_day = first_date
 
-	out_file = open("file/"+str(x)+".txt","w")
-	for y in range(1,8):
+for x in range(1, 365):
+    new_day = (new_day + day_delta)
+    new_baseday = datetime.datetime.combine(new_day, ttime)
+    new_baseday = (new_baseday + hour_delta + hour_delta)
 
-		new_baseday=(new_baseday+hour_delta)
-		print_format(new_baseday)
+    out_file = open("file/" + str(x) + ".txt", "w")
+    for y in range(1, 8):
 
-		out_file.write(formatter(new_baseday)+"\n")
-	out_file.close()
+        new_baseday = (new_baseday + hour_delta)
+        print_format(new_baseday)
+
+        out_file.write(formatter(new_baseday) + "\n")
+    out_file.close()
 
 print "Done."
-
